f5c10834ae0fbaa5e3247cb2a8116855c4d801b0
Mergen Battur <mergen@chowlyinc.com>
2021-06-23 10:37:24 -0500

    Stopping at minute 44

---
+++ src/components/Tasks.js
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const Tasks = () => {
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Doc apt',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Nba',
+        day: 'Feb 5th at 3:30pm',
+        reminder: false
+      }
+    ]
+  )
+
+  return (
+    <>
+      {tasks.map((task) =>
+      (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  )
+}
+
+export default Tasks
